1a7d58c39cd4797b1920eaf226ceaaea46a4d765
chckn9 <thomasyeoh9@gmail.com>
2025-12-29 15:12:49 -0800

    Fix error

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,6 @@ export { auth as middleware } from '@/lib/auth'
 
 export const config = {
   matcher: [
-    '/((?!login|api/auth|_next/static|_next/image|favicon.ico|demo.mp4|octopus.svg|.*\\.(svg|png|jpg|jpeg|gif|ico|mp4|webp)).*)',
+    '/(?:.*)(?<!login|api/auth|_next/static|_next/image|favicon.ico|demo.mp4|octopus.svg|.*\\.(svg|png|jpg|jpeg|gif|ico|mp4|webp))',
   ],
 }
